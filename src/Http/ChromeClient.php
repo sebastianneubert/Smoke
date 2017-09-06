27fd09e170c56083ba262725dec330bffc69bef2
phmLabs <nils.langner@phmlabs.com>
2017-09-06 08:41:55 +0200

    FileCache

--- src/Http/ChromeClient.php
+++ src/Http/ChromeClient.php
@@ -3,12 +3,35 @@
 namespace whm\Smoke\Http;
 
 use phm\HttpWebdriverClient\Http\Client\Chrome\ChromeClient as phmChromeClient;
+use phm\HttpWebdriverClient\Http\Client\Decorator\FileCacheDecorator;
+use phm\HttpWebdriverClient\Http\Client\HttpClient;
+use Psr\Http\Message\RequestInterface;
 
-class ChromeClient extends phmChromeClient
+class ChromeClient implements HttpClient
 {
+    /**
+     * @var phmChromeClient
+     */
+    private $chromeClient;
+
     public function init($host = 'localhost', $port = 4444)
     {
-        $this->setWebdriverHost($host);
-        $this->setWebdriverPort($port);
+        $chromeClient = new phmChromeClient($host, $port);
+        $this->chromeClient = new FileCacheDecorator($chromeClient);
+    }
+
+    public function sendRequest(RequestInterface $request)
+    {
+        return $this->chromeClient->sendRequest($request);
+    }
+
+    public function sendRequests(array $requests)
+    {
+        return $this->chromeClient->sendRequests($requests);
+    }
+
+    public function getClientType()
+    {
+        return $this->chromeClient->getClientType();
     }
 }
