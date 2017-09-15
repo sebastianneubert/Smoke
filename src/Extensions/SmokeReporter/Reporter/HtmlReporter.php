f02e4f065391f6b1219a5e243bf3d7a65f4ec453
Sebastian Neubert <sebastian.neubert@bauerxcel.de>
2017-09-15 15:39:20 +0200

    add HTML Reporter

---
+++ src/Extensions/SmokeReporter/Reporter/HtmlReporter.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace whm\Smoke\Extensions\SmokeReporter\Reporter;
+
+use Symfony\Component\Console\Output\OutputInterface;
+use whm\Smoke\Config\Configuration;
+use whm\Smoke\Extensions\SmokeResponseRetriever\Retriever\Retriever;
+use whm\Smoke\Rules\CheckResult;
+
+
+class HtmlReporter implements Reporter
+{
+    private $results = [];
+    /**
+     * @var Configuration
+     */
+    private $templateDir;
+    private $templateFile;
+    private $resultFile;
+
+    /**
+     * @var OutputInterface
+     */
+    private $output;
+
+    public function init(OutputInterface $_output, $resultFile, $templateDir = null, $templateFile = 'index.html.twig')
+    {
+        $this->resultFile = $resultFile;
+
+        if ($templateDir === null) {
+            $this->templateDir = __DIR__ . '/templates';
+        } else {
+            $this->templateDir = $templateDir;
+        }
+
+        $this->templateFile = $templateFile;
+        $this->output = $_output;
+    }
+
+    /**
+     * @param CheckResult[] $results
+     */
+    public function processResults($results)
+    {
+        foreach ($results as $result) {
+            $this->results[] = [
+                'url' => $result->getResponse()->getUri(),
+                'rule' => $result->getRuleName(),
+                'message' => $result->getMessage(),
+                'status' => $result->getStatus(),
+            ];
+        }
+    }
+
+    public function finish()
+    {
+        $loader = new \Twig_Loader_Filesystem($this->templateDir);
+        $twig = new \Twig_Environment($loader);
+
+        $html = $twig->render($this->templateFile, [
+            'results' => $this->results,
+        ]);
+
+        if (!file_exists(dirname($this->resultFile))) {
+            mkdir(dirname($this->resultFile));
+        }
+
+        if (!file_put_contents($this->resultFile, $html)) {
+            $this->output->writeln("<error>HTML Reporter extension: Could not write result file to " . $this->resultFile ."</error>");
+        } else {
+            $this->output->writeln("<info>HTML Reporter extension:</info> Result file written to " . $this->resultFile);
+        }
+    }
+
+}
